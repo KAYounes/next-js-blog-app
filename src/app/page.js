03e5fb034663ce5616a0e7c8609e5d9dc87c2516
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2024-04-24 08:33:27 +0200

    General changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+import ThemeButtons from '@/components/ThemeButtons/ThemeButtons';
 
 export default function Home() {
-  return (<><h1>Next App: Default Template</h1></>);
+  return (
+    <>
+      <ThemeButtons></ThemeButtons>
+    </>
+  );
 }
